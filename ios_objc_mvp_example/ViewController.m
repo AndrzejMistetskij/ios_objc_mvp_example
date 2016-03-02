5a7ef6945df72eed90e5a6d756d244c026d08b71
Luis Sánchez-Beato Frates <luis.sbeato@macbook-luis.local>
2016-03-02 10:42:26 +0100

    Initial Commit

---
+++ ios_objc_mvp_example/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ios_objc_mvp_example
+//
+//  Created by Luis Sánchez-Beato Frates on 2/3/16.
+//  Copyright © 2016 Luis Sánchez-Beato Frates. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
